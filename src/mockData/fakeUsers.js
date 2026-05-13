ad4bf653e2507b1c1476c062b773417faaec879a
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-05-13 15:15:24 +0700

    Set server & test

---
+++ src/mockData/fakeUsers.js
@@ -0,0 +1,5 @@
+export const users = [
+  { id: "1", username: "Palm", email: "test@example.com", password: "1234" },
+  { id: "2", username: "Moon", email: "test@example.com", password: "1234" },
+  { id: "3", username: "Sun", email: "test@example.com", password: "1234" },
+];
