56b2cb613a6023d00913cc3064ce7d50775f9832
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-05-15 11:10:40 +0700

    Create User mofel from userSchema

---
+++ src/modules/users/user.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, trim: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, minlength: 8, select: false },
+  },
+
+  { timestamps: true },
+);
+const User = mongoose.model("User", userSchema);
