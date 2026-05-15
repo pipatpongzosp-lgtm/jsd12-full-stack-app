0465740be2e4b5150c2f167a1d11abb04b20c974
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-05-15 10:22:43 +0700

    add mockup products

---
+++ src/mockData/fakeProducts.js
@@ -0,0 +1,23 @@
+export const Products = [
+  {
+    id: "1",
+    type: "softdrink",
+    productname: "Cola Coca",
+    price: "30 ",
+    qty: "5",
+  },
+
+  {
+    id: "2",
+    type: "softdrink",
+    productname: "Lerflarua",
+    price: "30 ",
+    qty: "5",
+  },
+
+  { id: "3",
+    type: "softdrink",
+    productname: "sticky",
+    price: "30 ",
+    qty: "5" },
+];
